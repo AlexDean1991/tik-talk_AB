2ffd1397370daed9f2425cf2623550bdbd13bbfe
Alexander_Berdnikov <alexander_berdnikov@mail.ru>
2025-01-09 17:28:17 +0300

    Changes LP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
-import {ProfileService} from './data/services/profile.service';
-import {Profile} from './data/interface/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent],
+  imports: [ProfileCardComponent, RouterOutlet],
 
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService = inject(ProfileService)
-  profiles: Profile[] = []
 
-  constructor() {
-    this.profileService.getTestAccounts()
-      .subscribe(val=> {
-        this.profiles = val
-      })
-  }
 }
