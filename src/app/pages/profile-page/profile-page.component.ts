6e20f69dcec45df8386a51fa0b24e73523a49ae2
Alexander_Berdnikov <alexander_berdnikov@mail.ru>
2025-01-10 10:22:49 +0300

    Сайдбар и Layout

---
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-page',
+  imports: [],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss'
+})
+export class ProfilePageComponent {
+
+}
