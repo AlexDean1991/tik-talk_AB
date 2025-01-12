30413f57ef1982526bc9796e1349c1776833996e
Alexander Berdnikov <alexander_berdnikov@mail.ru>
2025-01-12 22:07:14 +0300

    Рефреш Токен и начало сигналов

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,18 +1,58 @@
-import {HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+// @ts-ignore
+
+import {HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
+import {catchError, switchMap, throwError} from 'rxjs';
 
-export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+let isRefreshing = false
+
+  export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService)
   const token = inject(AuthService).token
+
 
   // console.log(req)
 if (!token) return next(req)
 
-  req = req.clone( {
+if (isRefreshing) {
+  return refreshAndProceed(authService, req, next)
+}
+
+  return next(addToken(req,token))
+    .pipe(
+      catchError(error => {
+        if (error.status === 403) {
+          return refreshAndProceed(authService, req, next)
+        }
+
+        return throwError(error)
+      })
+    )
+}
+
+const refreshAndProceed = (
+  authService: AuthService,
+  req: HttpRequest<any>,
+  next: HttpHandlerFn
+) => {
+  if (!isRefreshing) {
+    return authService.refreshAuthToken()
+      .pipe(
+        switchMap(res => {
+          isRefreshing = false
+          return next(addToken(req, res.access_token))
+        })
+      )
+  }
+
+  return next(addToken(req, authService.token!))
+}
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+  return req.clone({
     setHeaders: {
       Authorization: `Bearer ${token}`
     }
   })
-
-  return next(req)
 }
--- src/app/data/services/profile.service.ts
+++ src/app/data/services/profile.service.ts
@@ -13,6 +13,11 @@ export class ProfileService {
   getTestAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}/account/test_accounts`)
   }
+
+  getMe() {
+    return this.http.get<Profile>(`${this.baseApiUrl}/account/me`)
+  }
+
 }
 
 
