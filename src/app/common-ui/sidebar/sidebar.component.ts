6b7f996c1d3bf0ebe1ba073fdea57acaf0dbb088
Alexander_Berdnikov <alexander_berdnikov@mail.ru>
2025-01-13 15:20:44 +0300

    Подписчики rev 2.0

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {SvgIconComponent} from '../svg-icon/svg-icon.component';
 import {NgForOf} from '@angular/common';
 import {SubscriberCardComponent} from './subscriber-card/subscriber-card.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIconComponent, NgForOf, SubscriberCardComponent],
+  imports: [SvgIconComponent, NgForOf, SubscriberCardComponent, RouterLink],
   templateUrl: './sidebar.component.html',
   standalone: true,
   styleUrl: './sidebar.component.scss'
