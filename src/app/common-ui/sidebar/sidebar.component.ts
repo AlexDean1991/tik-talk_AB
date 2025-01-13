4e4c29a557aa56628d7179c7e422b1156317387d
Alexander_Berdnikov <alexander_berdnikov@mail.ru>
2025-01-13 11:30:32 +0300

    Доделал верхнее меню

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import {SvgIconComponent} from '../svg-icon/svg-icon.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, NgForOf],
   templateUrl: './sidebar.component.html',
   standalone: true,
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+    menuItems = [
+      {
+        label: 'Моя страница',
+        icon: 'home',
+        link: ''
+      },
+      {
+        label: 'Чаты',
+        icon: 'chat',
+        link: 'chat'
+      },
+      {
+        label: 'Поиск',
+        icon: 'search',
+        link: 'search'
+      }
+    ]
 }
