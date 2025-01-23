56ef7bd3acd09e59704dc49c917d05ea2aae3f82
Alexander_Berdnikov <alexander_berdnikov@mail.ru>
2025-01-23 16:51:14 +0300

    Наладил переключение между подписчиками. Вот комментарий гпт
    Когда вы передаете путь в виде ['.profile', subscriber.id], Angular интерпретирует его как относительный маршрут от текущего URL (/profile/me).
    Вместо этого нужно явно указать абсолютный маршрут.
    
    <a class="subscriber-avatar-btn" [routerLink]="['/profile', subscriber.id]">
    Это создаст абсолютный маршрут вида /profile/123.
    Почему это важно?
    Если вы хотите, чтобы URL всегда начинался с /profile, используйте абсолютные маршруты (начинаются с /). Angular не будет пытаться добавлять сегменты из текущего пути.

--- src/app/common-ui/directives/dnd.directive.ts
+++ src/app/common-ui/directives/dnd.directive.ts
@@ -1,40 +1,35 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
+  standalone: true,
   selector: '[dnd]'
 })
-
-
 export class DndDirective {
 
   @HostBinding('class.fileover')
-  fileover = false
+  fileover = false;
 
-}
   @HostListener('dragover', ['$event'])
   onDragOver(event: DragEvent) {
     event.preventDefault();
     event.stopPropagation();
-
-    console.log(event)
+    console.log(event);
+    this.fileover = true;
   }
 
-  export class DndDirective {
   @HostListener('dragleave', ['$event'])
   onDragLeave(event: DragEvent) {
     event.preventDefault();
     event.stopPropagation();
-
-    console.log(event)
+    console.log(event);
+    this.fileover = false;
   }
 
-  export class DndDirective {
   @HostListener('drop', ['$event'])
   onDrop(event: DragEvent) {
     event.preventDefault();
     event.stopPropagation();
-
-    console.log(event)
+    console.log(event);
+    this.fileover = false;
   }
-
 }
